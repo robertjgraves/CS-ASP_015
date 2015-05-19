0a7ea6392fd3136a3325b970af9c8c217a03704e
Robert Graves <robertjgraves@gmail.com>
2015-05-19 09:49:51 -0700

    Initial commit

---
+++ CS-ASP_015/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_015
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            // http://is.gd/timespan
+            // Days.Hours.Minutes:Seconds.Milliseconds
+            TimeSpan myTimeSpan = TimeSpan.Parse("1.2:3:30.5");
+
+            DateTime myBirthday = DateTime.Parse("2/27/1979");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+
+            resultsLabel.Text = myAge.TotalDays.ToString();
+        }
+    }
+}
